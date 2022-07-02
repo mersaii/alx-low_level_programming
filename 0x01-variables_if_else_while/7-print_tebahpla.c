25592ed7796b35a5af8c00b9023fa9461914bfc0
Mersaii <mercyadeige@gmail.com>
2022-07-01 17:02:56 -0700

    0,1,4,7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,11 +4,11 @@
  * followed by a new line,
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 	char ch;
 
-	for (ch = 'Z'; ch >= 'a'; ch--)
+	for (ch = 'z'; ch >= 'a'; ch--)
 	{
 		putchar(ch);
 	}
